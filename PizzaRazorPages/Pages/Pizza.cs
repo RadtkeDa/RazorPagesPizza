d2b67e6c0160f8f5ee8081194486b56b3f5461be
Daniel Radtke <Radtked@roverkids.org>
2024-02-05 10:20:32 -0500

    Initial commit

---
+++ PizzaRazorPages/Pages/Pizza.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaRazorPages.Pages
+{
+    public class Pizza
+    {
+        using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesPizza.Models;
+
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+
+        [Range(0.01, 9999.99)]
+        public decimal Price { get; set; }
+    }
+}
+}
